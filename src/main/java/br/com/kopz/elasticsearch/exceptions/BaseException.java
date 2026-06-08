df683f65ef8b2fbf662263601cd94d0ee7f435f3
kopz9 <gustavoeduardo438@gmail.com>
2026-06-08 10:47:35 -0300

    feat: exceptions

---
+++ src/main/java/br/com/kopz/elasticsearch/exceptions/BaseException.java
@@ -0,0 +1,18 @@
+package br.com.kopz.elasticsearch.exceptions;
+
+public class BaseException extends RuntimeException {
+  public BaseException() {
+  }
+
+  public BaseException(String message) {
+    super(message);
+  }
+
+  public BaseException(String message, Throwable cause) {
+    super(message, cause);
+  }
+
+  public BaseException(Throwable cause) {
+    super(cause);
+  }
+}
